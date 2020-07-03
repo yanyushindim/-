467e79a5cec1181208ca47e3a31057589e6eb84b
Dmitry <yanyushin.dim@gmail.com>
2020-07-03 20:28:19 +0300

    торой практикум

--- script.js
+++ script.js
@@ -13,10 +13,25 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последний просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
+
     if (a != null && b != null && a !='' && b != '' && a.length < 50) {
-        
+    personalMovieDB.movies[a] = b;
+    console.log('done');    
+    } else {
+        console.log('error');
+        i--;
     }
-    personalMovieDB.movies[a] = b;       
 }
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотренно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+    
 
 console.log(personalMovieDB);
